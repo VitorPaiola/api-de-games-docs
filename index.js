3f493a78e5b1e85c29d108555999e132c306d7cb
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-07-08 13:06:08 -0300

    RESTful com HATEOAS

--- index.js
+++ index.js
@@ -90,8 +90,28 @@ var DB = {
 }
 
 app.get("/games", auth, (req, res) => {
+
+    var HATEOAS = [
+        {
+            href: "http://localhost:45678/game/0",
+            method: "DELETE",
+            rel: "delete_game"
+        },
+        {
+            href: "http://localhost:45678/game/0",
+            method: "GET",
+            rel: "get_game"
+        },
+        {
+            href: "http://localhost:45678/auth",
+            method: "POST",
+            rel: "login"
+        }
+
+    ]
+
     res.statusCode = 200;
-    res.json(DB.games);
+    res.json({games: DB.games, _links: HATEOAS});
 });
 
 app.get("/game/:id", auth, (req, res) => {
@@ -101,11 +121,35 @@ app.get("/game/:id", auth, (req, res) => {
 
         var id = parseInt(req.params.id)
 
+        var HATEOAS = [
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "DELETE",
+                rel: "delete_game"
+            },
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "PUT",
+                rel: "edit_game"
+            },
+            {
+                href: "http://localhost:45678/game/"+id,
+                method: "GET",
+                rel: "get_game"
+            },
+            {
+                href: "http://localhost:45678/games",
+                method: "GET",
+                rel: "get_all_games"
+            }
+    
+        ]
+
         var game = DB.games.find(g => g.id == id)
 
         if (game != undefined) {
             res.statusCode = 200
-            res.json(game)
+            res.json({game, _links: HATEOAS})
         } else {
             res.sendStatus(404)
         }
